bc2276800f5add176bad0d70ea092b848258c09b
epicodus <student@epicodus.com>
2014-02-24 16:59:38 -0800

    address book with partial update aabilities

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,10 @@ var Contact = {
 };
 
 $(document).ready(function() {
+  var contactToEdit;
+
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
-
-
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
     var inputtedAddress = $("input#new-address").val();
@@ -20,13 +20,28 @@ $(document).ready(function() {
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
     $(".contact").last().click(function() {
+      contactToEdit = newContact;
       $("#show-contact").show();
       $("#show-contact h2").text(newContact.fullName());
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
       $(".address").text(newContact.address);
-
     });
+
+    $("button#edit").last().click(function() {
+      event.preventDefault();
+        $('#edit-contact').show();
+        $('input#edit-first-name').val(contactToEdit.firstName);
+        $('input#edit-last-name').val(contactToEdit.lastName);
+        $('input#edit-address').val(contactToEdit.address);
+        $('#show-contact').hide();
+    });
+
+    $("form#edit-contact").submit(function(event) {
+      event.preventDefault();
+      
+    });
+
     this.reset();
   });
 });
